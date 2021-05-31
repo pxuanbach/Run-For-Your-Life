d0ac6c0dadc37d6be658b4b7c1c54c8c7ee207be
James1108Nguyen <71433464+James1108Nguyen@users.noreply.github.com>
2021-05-31 11:17:19 +0700

    BackendAPI

---
+++ app.js
@@ -0,0 +1,47 @@
+
+const express = require('express')
+const app = express()
+const mongoose = require("mongoose")
+const morgan=require('morgan')
+
+// để xem log web521
+app.use(morgan("tiny"));
+
+//Using enviroment variable 
+require("dotenv").config();
+
+
+//middleware
+app.use(express.urlencoded()); // Parse URL-encoded bodies
+app.use(express.json()); //to support JSON encode
+
+var cors = require('cors');
+app.use(cors());
+
+var userRouter = require("./router/userRouter")
+app.use("/api/users",userRouter);
+
+const connectString=process.env.connectString;
+// kết nối database
+mongoose
+  .connect(connectString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((value) => {
+    console.log("database connect success");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+
+
+// để định nghĩa port 3000
+
+const PORT = process.env.PORT || 3000;
+var server = app.listen(PORT, function () {
+  var port = server.address().port;
+  console.log("Express is working on port", port);
+});
+
+
+
