7455eefd74ae17d8128ecbb3db5719b2202e7cfd
James1108Nguyen <71433464+James1108Nguyen@users.noreply.github.com>
2021-06-03 17:17:34 +0700

    k
    k

--- router/userRouter.js
+++ router/userRouter.js
@@ -3,6 +3,10 @@ const express = require("express")
 const  router = express.Router()
 const { User } = require("../models/user")
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
+
 router.get('/',async function(req,res){
     var users = await User.find();
    if (users) {
@@ -18,25 +22,26 @@ router.post("/login", async function(req,res){
     let user = await User.findOne({username : req.body.username})
     if (!req.body.username) {
         return res.status(400).send("Vui lòng nhập tài khoản");
-      }
+      }  
       if (!req.body.password) {
         return res.status(400).send("Vui lòng nhập mật khẩu");
     }
     if (!user) {
         return res.status(400).send("Tài khoản không hợp lệ");
       }
-    if(user && (req.body.password==user.password)){ 
+    if(user && bcrypt.compareSync(req.body.password,user.password)){ 
         return res.status(200).send("Đăng nhập thành công")
     }
 
 })
+
 
 //register
 router.post('/register', async function(req,res){
     let user = User({
         email: req.body.email,
         username: req.body.username,
-        password:req.body.password,
+        password: bcrypt.hashSync(req.body.password, salt),
         phone: req.body.phone,
         sex: req.body.sex,
         address: req.body.address,
