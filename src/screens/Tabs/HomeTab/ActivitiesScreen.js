84b6455f9a7bce2ae06871ff605456b710b65397
voquocminh <19521857@gm.uit.edu.vn>
2021-06-14 22:33:16 +0700

    update xơ xơ

--- src/screens/Tabs/HomeTab/ActivitiesScreen.js
+++ src/screens/Tabs/HomeTab/ActivitiesScreen.js
@@ -2,15 +2,21 @@ import React, {useState} from 'react';
 import { SafeAreaView } from 'react-native';
 import {Text, View, ScrollView, Dimensions, StyleSheet, TouchableOpacity} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import ActivitiesCard from '../../../components/ActivitiesCard';
 
 function ActivitiesScreen({navigation}){
     return(
         <SafeAreaView>
             <StatusBar style='auto'/>
             <View>
-                <Text>
-                    activities here !
-                </Text>
+                <ActivitiesCard
+                    dateTime="Monday Evening"
+                    map="map nè"
+                    distance={(Math.random()*100).toFixed(0)}
+                    time={(Math.random()*100).toFixed(0)}
+                    avgPace={(Math.random()*100).toFixed(0)}
+                    calories={(Math.random()*100).toFixed(0)}
+                    />
             </View>
         </SafeAreaView>
 
